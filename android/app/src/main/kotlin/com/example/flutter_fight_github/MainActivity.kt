e0be6ece10f9b29e012d481d47451528b24c9533
郑晓波 <zhengxiaobo@jianbing.com>
2020-04-07 18:45:20 +0800

    update

---
+++ android/app/src/main/kotlin/com/example/flutter_fight_github/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fight_github
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/flutterfightgithub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterfightgithub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
